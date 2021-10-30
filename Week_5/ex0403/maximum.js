27cb0fd2e4c370c165a029302b0bf813d890c1df
cfowle4786 <cfowle4786@my.sullivan.edu>
2021-10-29 19:48:50 -0500

    Finished ex0403

---
+++ Week_5/ex0403/maximum.js
@@ -0,0 +1,7 @@
+var a = prompt("Enter number one:");
+var b = prompt("Enter number two:");
+var c = prompt("Enter number three:");
+
+document.write("The numbers are: "+a+","+b+", "+c+"<br/>")
+
+document.write("maximum number is: "+Math.max(a,b,c));
