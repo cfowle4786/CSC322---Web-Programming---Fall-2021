729b86a2daf17ed0eee9513545ae721156ba724f
cfowle4786 <cfowle4786@my.sullivan.edu>
2021-11-07 07:40:42 -0600

    Finished everything except color.. Please help

---
+++ Week_6/Assignment6.2/madLib/madLib.js
@@ -0,0 +1,23 @@
+var animal = prompt("Enter an animal:");
+var country = prompt("Enter a country:");
+var pluralnoun = prompt("Enter a plural noun:");
+var food = prompt("Enter a food:");
+var screen = prompt("Enter a device with a screen:");
+var noun = prompt("Enter a noun:");
+var verb = prompt("Enter a verb:");
+var verb2 = prompt("Enter a verb:");
+var adjective = prompt("Enter an adjective:");
+var emotion = prompt("Enter an emotion:");
+
+
+
+document.write
+            (
+                "The Majestic " + animal + " has roamed the forests of " + country + 
+                " for thousands of years. Today, she wanders in search of " + pluralnoun + 
+                ". She must find food to survive. While hunting for " + food +
+                ", she found a " + screen + "hidden behind a " + noun +
+                ". She has never seen anything likd this before. What will she do? With the device in her teeth, she tries to " + verb + 
+                ", but nothing happens. She takes it back to her family. When her family sees it they quickly " + verb2 +
+                " Soon, the device becomes " + adjective + ", and the family decides to put it back where they found it. They are filled with " + emotion + "."
+            )
